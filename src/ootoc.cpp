97422ffe2173da23e67a3751ac167a340485a727
ElonH <elonhhuang@gmail.com>
2020-03-06 01:36:30 +0800

    fix data

--- src/ootoc.cpp
+++ src/ootoc.cpp
@@ -68,25 +68,25 @@ bool TarParser::Parse()
     {
         // th_print(tar);
         // th_print_long_ls(tar);
-        if (TH_ISREG(tar) && tar_skip_regfile(tar) != 0)
-            return false;
         if (!TH_ISREG(tar))
             continue;
         string inner_path = th_get_pathname(tar);
-        auto offset = tar->offset;
-        auto fsize = th_get_size(tar);
+        auto sta = tar->offset;
+        auto end = sta -1 + th_get_size(tar);
         cout << inner_path << endl;
         cout << tar->offset << endl;
         ss.str("");
-        ss << offset;
+        ss << sta;
         node[inner_path]["start"] = ss.str();
         ss.str("");
-        ss << fsize;
-        node[inner_path]["size"] = ss.str();
+        ss << end;
+        node[inner_path]["end"] = ss.str();
         // regex reg(".*?/Packages.gz$");
         // auto is_exist = regex_match(inner_path, reg);
         // if (is_exist)
         //     cout << "true" << endl;
+        if (TH_ISREG(tar) && tar_skip_regfile(tar) != 0)
+            return false;
     }
     ss.str("");
     ss << node;
