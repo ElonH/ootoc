8d9ac7166d7ca8d0331fb7357beb613b0d094c14
ElonH <elonhhuang@gmail.com>
2020-03-07 17:46:01 +0800

    update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,34 @@
+#include "CLI/CLI.hpp"
 #include "ootoc.h"
 
-int main(int argc, char *argv[])
+using namespace ootoc;
+
+int main(int argc, char **argv)
 {
+    CLI::App app;
+
+    string addr = "127.0.0.1";
+    app.add_option("--addr", addr);
+
+    int port = 21730;
+    app.add_option("--port", port)->check(CLI::Range(1 << 15));
+
+    string url;
+    app.add_option("--url", url)->required();
+
+    string aux;
+    app.add_option("--aux", aux)->required();
+
+    CLI11_PARSE(app, argc, argv);
+
+    fstream auxstm(aux, ios::in);
+    string aux_content((std::istreambuf_iterator<char>(auxstm)),
+                       (std::istreambuf_iterator<char>()));
+    auxstm.close();
+
+    OpkgServer server;
+    server.setRemoteTar(url, aux_content);
+    server.setServer(addr, port);
+    server.Start();
+    return 0;
 }
