b0cfa9e6797e5db87534470d860a782a569aa279
ElonH <elonhhuang@gmail.com>
2020-03-08 01:11:10 +0800

    sub-cmd: server, parse

--- src/main.cpp
+++ src/main.cpp
@@ -7,28 +7,47 @@ int main(int argc, char **argv)
 {
     CLI::App app;
 
+    auto appSvr = app.add_subcommand("server", "start ootoc server");
+    auto appPsr = app.add_subcommand("parse", "parse tar archive file");
+
     string addr = "127.0.0.1";
-    app.add_option("--addr", addr);
-
+    appSvr->add_option("--addr", addr);
     int port = 21730;
-    app.add_option("--port", port)->check(CLI::Range(1 << 15));
-
+    appSvr->add_option("--port", port)->check(CLI::Range(1 << 15));
     string url;
-    app.add_option("--url", url)->required();
-
+    appSvr->add_option("--url", url)->required();
     string aux;
-    app.add_option("--aux", aux)->required();
+    appSvr->add_option("--aux", aux)->required();
+    appPsr->add_option("--in,-i", aux, "tar file")->required();
+
+    string out;
+    appPsr->add_option("--out,-o", out, "output result[yaml file]")->required();
+
+    // app.callback([&]() { cout << app.help() << endl; });
+    appSvr->callback([&]() {
+        fstream auxstm(aux, ios::in);
+        string aux_content((std::istreambuf_iterator<char>(auxstm)),
+                           (std::istreambuf_iterator<char>()));
+        auxstm.close();
+
+        OpkgServer server;
+        server.setRemoteTar(url, aux_content);
+        server.setServer(addr, port);
+        server.Start();
+    });
+
+    appPsr->callback([&]() {
+        TarParser psr;
+        psr.Open(aux);
+        psr.Parse();
+        psr.Close();
+        fstream ofs(out, ios::out);
+        ofs << psr.GetOutput();
+        // cout << psr.GetOutput() << endl;
+        cout << "finished." << endl;
+        ofs.close();
+    });
 
     CLI11_PARSE(app, argc, argv);
-
-    fstream auxstm(aux, ios::in);
-    string aux_content((std::istreambuf_iterator<char>(auxstm)),
-                       (std::istreambuf_iterator<char>()));
-    auxstm.close();
-
-    OpkgServer server;
-    server.setRemoteTar(url, aux_content);
-    server.setServer(addr, port);
-    server.Start();
     return 0;
 }
