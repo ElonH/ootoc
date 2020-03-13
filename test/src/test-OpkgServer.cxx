8892d7c412414e3bd18d2e35e922fc90ed78c239
ElonH <elonhhuang@gmail.com>
2020-03-14 00:17:35 +0800

    :white_check_mark: update test

--- test/src/test-OpkgServer.cxx
+++ test/src/test-OpkgServer.cxx
@@ -10,12 +10,9 @@
 using namespace ootoc;
 TEST(OpkgServer, test)
 {
-    fstream auxstm("./test/data/Tar.yml", ios::in);
-    string content((std::istreambuf_iterator<char>(auxstm)),
-                   (std::istreambuf_iterator<char>()));
-    auxstm.close();
     OpkgServer s;
-    s.setRemoteTar("https://github.com/ElonH/ootoc/releases/download/test/Packages.tar", content);
+    s.setAuxUrl("", "./test/data/Tar.yml");
+    s.setRemoteTar("https://github.com/ElonH/ootoc/releases/download/test/Packages.tar");
     s.setServer("localhost", 21730);
     // s.Start();
 }
