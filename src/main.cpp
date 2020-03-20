49826b0a209f51feb320b7ba57fe1e386ed084ca
ElonH <elonhhuang@gmail.com>
2020-03-20 23:05:57 +0800

    :recycle: refactoring CLI

--- src/main.cpp
+++ src/main.cpp
@@ -11,45 +11,45 @@ int main(int argc, char **argv)
     auto appPsr = app.add_subcommand("parse", "parse tar archive file");
 
     string log_path = "";
-    appSvr->add_option("--log,-l", log_path, "log file, otherwise output log to console");
-    appPsr->add_option("--log,-l", log_path, "log file, otherwise output log to console");
+    appSvr->add_option("--log,-l", log_path, "log file, otherwise output log to console", true);
+    appPsr->add_option("--log,-l", log_path, "log file, otherwise output log to console", true);
     string addr = "127.0.0.1";
-    appSvr->add_option("--addr", addr);
+    appSvr->add_option("--addr", addr, "local address", true);
     int port = 21730;
-    appSvr->add_option("--port", port);
-    string url;
-    appSvr->add_option("--url", url)->required();
-    string aux;
-    appSvr->add_option("--aux", aux)->required();
-    appPsr->add_option("--in,-i", aux, "tar file")->required();
-    string online_aux = "";
-    appSvr->add_option("--aux-url", online_aux);
+    appSvr->add_option("--port", port, "local port", true);
+    string tar_url;
+    appSvr->add_option("--tar-url", tar_url)->required();
+    string aux_url;
+    appSvr->add_option("--aux-url", aux_url)->required();
+    string tar_path;
+    appPsr->add_option("--in,-i", tar_path, "tar file")->required();
 
     string sub_path = "";
-    appSvr->add_option("--subscription", sub_path, "subscription file path");
+    appSvr->add_option("--subscription", sub_path, "subscription file path. Default, disable output subscription content.", true);
     string out;
     appPsr->add_option("--out,-o", out, "output result[yaml file]")->required();
 
-    // app.callback([&]() { cout << app.help() << endl; });
+    app.callback([&]() { cout << app.help() << endl; });
     appSvr->callback([&]() {
         Logger::start(log_path);
         OpkgServer server;
-        server.setSubscription(sub_path);
-        server.setAuxUrl(online_aux, aux);
-        server.setRemoteTar(url);
-        server.setServer(addr, port);
+        server.tar_url = tar_url;
+        server.aux_url = aux_url;
+        server.local_addr = addr;
+        server.local_port = port;
+        server.subscription_path = sub_path;
+        server.Check();
         server.Start();
     });
 
     appPsr->callback([&]() {
         Logger::start(log_path);
         TarParser psr;
-        psr.Open(aux);
+        psr.Open(tar_path);
         psr.Parse();
         psr.Close();
         fstream ofs(out, ios::out);
         ofs << psr.GetOutput();
-        // cout << psr.GetOutput() << endl;
         cout << "finished." << endl;
         ofs.close();
     });
